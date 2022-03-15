63950042ec5fc53486a2f47b5b53448392de036e
Anna Archut <anna.archut@hcl.com>
2022-03-15 08:21:18 -0400

    section 30

--- src/main/java/com/luv2code/eccomerce/config/MyDataRestConfig.java
+++ src/main/java/com/luv2code/eccomerce/config/MyDataRestConfig.java
@@ -1,6 +1,8 @@
 package com.luv2code.eccomerce.config;
 
 import java.util.ArrayList;
+
+
 import java.util.List;
 import java.util.Set;
 
@@ -8,6 +10,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.metamodel.EntityType;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
@@ -16,13 +19,19 @@ import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 import com.luv2code.eccomerce.entity.Country;
+import com.luv2code.eccomerce.entity.Order;
 import com.luv2code.eccomerce.entity.Product;
 import com.luv2code.eccomerce.entity.ProductCategory;
 import com.luv2code.eccomerce.entity.State;
 
+
+
 @Configuration
 public class MyDataRestConfig implements RepositoryRestConfigurer {
 
+	@Value("${allowed.origins}")
+	private String[] theAllowedOrigins;
+	
 	private EntityManager entityManager;
 
 	@Autowired
@@ -34,15 +43,21 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
 	public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
 		// TODO Auto-generated method stub
 
-		HttpMethod[] theUnsupportedActions = { HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE };
+		HttpMethod[] theUnsupportedActions = { HttpMethod.PUT, HttpMethod.POST, 
+											HttpMethod.DELETE, HttpMethod.PATCH};
 
 		// disable Http Methods for Product & Product Category: PUT, POST, DELETE
 		disableHttpMethods(Product.class, config, theUnsupportedActions);
 		disableHttpMethods(ProductCategory.class, config, theUnsupportedActions);
 		disableHttpMethods(Country.class, config, theUnsupportedActions);
 		disableHttpMethods(State.class, config, theUnsupportedActions);
+		disableHttpMethods(Order.class, config, theUnsupportedActions);
 
 		exposeIds(config);
+		
+		//configure cors mapping
+		
+		cors.addMapping(config.getBasePath() + "/**").allowedOrigins(theAllowedOrigins);
 	}
 
 	private void disableHttpMethods(Class theClass, RepositoryRestConfiguration config, HttpMethod[] theUnsupportedActions) {
--- src/main/java/com/luv2code/eccomerce/dao/ProductRepository.java
+++ src/main/java/com/luv2code/eccomerce/dao/ProductRepository.java
@@ -3,12 +3,13 @@ package com.luv2code.eccomerce.dao;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.luv2code.eccomerce.entity.Product;
 
-@CrossOrigin("http://localhost:4200")
+@RepositoryRestResource
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
 	// find by is the query method, category id is the match
